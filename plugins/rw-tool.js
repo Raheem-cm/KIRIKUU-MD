f6a2c2ee6410d2c710284151cee3459009331f81
Raheem-cm <nyoniraheem@gmail.com>
2025-06-15 20:04:03 +0300

    Update rw-tool.js

--- plugins/rw-tool.js
+++ plugins/rw-tool.js
@@ -17,7 +17,7 @@ cmd({
     const { data } = await axios.get(apiUrl);
     
     if (data.status && data.imgUrl) {
-      const caption = `🌌 *Random Wallpaper: ${query}*\n\n> *© Powered by Nexus Tech*`;
+      const caption = `🌌 *Random Wallpaper: ${query}*\n\n> *© Powered by raheem xmd*`;
       await conn.sendMessage(from, { image: { url: data.imgUrl }, caption }, { quoted: m });
     } else {
       reply(`❌ No wallpaper found for *"${query}"*.`);
