cd86f1a26c6cdf725f02edc2d6b02475ddcb6000
Raheem-cm <nyoniraheem@gmail.com>
2025-06-15 16:23:16 +0300

    Update tools-fun.js

--- plugins/tools-fun.js
+++ plugins/tools-fun.js
@@ -18,7 +18,7 @@ cmd({
       return reply("❌ Failed to fetch a joke. Please try again.");
     }
 
-    const jokeMessage = `🤣 *Here's a random joke for you!* 🤣\n\n*${joke.setup}*\n\n${joke.punchline} 😆\n\n> *© Powered by ELLY TECH*`;
+    const jokeMessage = `🤣 *Here's a random joke for you!* 🤣\n\n*${joke.setup}*\n\n${joke.punchline} 😆\n\n> *© Powered by KIRIKUU MD*`;
 
     return reply(jokeMessage);
   } catch (error) {
