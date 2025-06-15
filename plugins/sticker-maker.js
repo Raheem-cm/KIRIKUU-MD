e8e66bb9867c2cbb5793eeea4477805de5db658e
Raheem-cm <nyoniraheem@gmail.com>
2025-06-15 19:50:29 +0300

    Update sticker-maker.js

--- plugins/sticker-maker.js
+++ plugins/sticker-maker.js
@@ -57,7 +57,7 @@ cmd(
     async (conn, mek, m, { quoted, args, q, reply, from }) => {
         if (!mek.quoted) return reply(`*Reply to any Image or Video, Sir.*`);
         let mime = mek.quoted.mtype;
-        let pack = Config.STICKER_NAME || "ELLY-TECH";
+        let pack = Config.STICKER_NAME || "KIRIKUU-MD";
         
         if (mime === "imageMessage" || mime === "stickerMessage") {
             let media = await mek.quoted.download();
